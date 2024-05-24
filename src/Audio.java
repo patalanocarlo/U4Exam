28a26139252a5370c81190d1dc962f770f495d86
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 10:29:34 +0200

    First possible Solution of audio Player

--- src/Audio.java
+++ src/Audio.java
@@ -1,2 +1,21 @@
-public class Audio {
+public class Audio  extends ElementoMultimediale{
+    private int durata;
+    private  int volume;
+    public Audio( String Titolo , int durata , int volume){
+        super(Titolo);
+        this.durata=durata;
+        this.volume=volume;
+    }
+    public void alzaVolume(){
+        volume++;
+    }
+    public void DiminuisciVolume(){
+        if (volume > 0) volume--;
+    }
+    @Override
+public void esegui(){
+        for (int i=0 ; i < durata;i++){
+            System.out.println(Titolo + " "+ "!".repeat(volume));
+        }
+    }
 }
