69b28e2bf59204e055ea047913d996177c32deba
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 11:30:11 +0200

    Possible final solution now time to test and check

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,40 @@ public class Main {
             System.out.println("Inserisci il tipo di elemento a tua scelta, ecco la lista comandi: (1:Immagine,2:Audio,3:Video)");
             int tipoElementoScelto= value.nextInt();
             value.nextLine();
+            System.out.println("Inserisci un Titolo che desideri:");
+            String TitoloDelelementoScelto=value.nextLine();
+            if (tipoElementoScelto==1){
+                System.out.println("Vedo che hai scelto l'Immagine.");
+                System.out.println("Inserisci la luminosità:");
+                int luminosità = value.nextInt();
+                elementi[i]=new Immagine(TitoloDelelementoScelto,luminosità);
+            } else if  (tipoElementoScelto == 2) {
+                System.out.println("Vedo che hai scelto l'Audio");
+                System.out.println("Inserisci la durata: ");
+                int durata = value.nextInt();
+                System.out.println("Inserisci il volume: ");
+                int volume = value.nextInt();
+                elementi[i] = new Audio(TitoloDelelementoScelto, durata, volume);
+            }else if (tipoElementoScelto == 3) {
+                System.out.println("Inserisci la durata: ");
+                int durata = value.nextInt();
+                System.out.println("Inserisci il volume: ");
+                int volume = value.nextInt();
+                System.out.println("Inserisci la luminosità: ");
+                int luminosita = value.nextInt();
+                elementi[i] = new Video(TitoloDelelementoScelto, durata, volume, luminosita);
+            }
+        }
+        int scelta;
+        do {
+            System.out.println("Inserisci il numero dell'elemento da eseguire (1-5) oppure 0 per finire: ");
+            scelta=value.nextInt();
+            if (scelta > 0 && scelta <= 5) {
+                ElementoMultimediale elemento = elementi[scelta - 1];
+                elemento.esegui();
+            }
+        } while (scelta != 0);
+      value.close();
         }
     }
-}
+
