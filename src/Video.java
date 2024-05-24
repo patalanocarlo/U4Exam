542bd770febba21776fb194fd5a627877514c1ae
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 10:42:21 +0200

    first part of video

--- src/Video.java
+++ src/Video.java
@@ -1,2 +1,12 @@
-public class Video {
+public class Video extends ElementoMultimediale {
+    private int durata;
+    private int volume;
+    private int luminosità;
+    public Video(String Titolo, int durata ,int volume , int luminosità){
+        super(Titolo);
+        this.durata=durata;
+        this.volume=volume ;
+        this.luminosità=luminosità;
+    }
+    public void alzaVolume
 }
