245751fd01f9dff3a332141e195ed974ce66abd1
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 10:15:57 +0200

    ElementoMultimediale implementation

--- src/ElementoMultimediale.java
+++ src/ElementoMultimediale.java
@@ -1,2 +1,7 @@
-public class ElementoMultimediale {
+public abstract class ElementoMultimediale {
+    protected String Titolo;
+    public ElementoMultimediale( String Titolo){
+        this.Titolo = Titolo;
+    }
+    public abstract void esegui()
 }
--- src/Immagine.java
+++ src/Immagine.java
@@ -1,2 +1,14 @@
-public class Immagine {
+public class Immagine extends ElementoMultimediale {
+    private int luminosità;
+
+    public Immagine(String Titolo , int luminosità){
+        super(Titolo);
+        this.luminosità=luminosità;
+    }
+    public void aumentaLuminosità(){
+        luminosità++;
+    }
+    public void  diminuisciLuminosità(){
+        luminosità--;
+    }
 }
