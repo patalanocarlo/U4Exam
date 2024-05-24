22144f4a37c291dbe2d450175883dc5cbe8da696
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 11:06:05 +0200

    First part of main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import  java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner value=new Scanner(System.in);
+        ElementoMultimediale[] elementi= new ElementoMultimediale[5];
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Inserisci il tipo di elemento a tua scelta, ecco la lista comandi: (1:Immagine,2:Audio,3:Video)");
+            int tipoElementoScelto= value.nextInt();
+            value.nextLine();
+        }
+    }
 }
