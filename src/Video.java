c8b6e0cac11482a0efcdb26aa73b01de2425361d
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-24 10:56:06 +0200

    First probably solution of Video

--- src/Video.java
+++ src/Video.java
@@ -8,5 +8,23 @@ public class Video extends ElementoMultimediale {
         this.volume=volume ;
         this.luminosità=luminosità;
     }
-    public void alzaVolume
+    public void alzaVolume(){
+        volume++;
+    }
+    public void abbassaVolume(){
+        if (volume > 0) volume--;
+    }
+
+    public void aumentaLuminosita() {
+        luminosità++;
+    }
+
+    public void diminuisciLuminosita() {
+        if (luminosità > 0) luminosità--;
+    }
+    public void esegui() {
+        for (int i = 0; i < durata; i++) {
+            System.out.println(Titolo + " " + "!".repeat(volume) + "*".repeat(luminosità));
+        }
+    }
 }
